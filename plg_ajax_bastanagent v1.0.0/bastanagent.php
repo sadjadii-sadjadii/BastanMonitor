4817ad0b91b9f4f06e50c47e33cf0285bedcee72
Mohsen sadjadii <149596567+sadjadii-sadjadii@users.noreply.github.com>
2026-07-30 16:01:04 +0330

    Add files via upload

--- plg_ajax_bastanagent v1.0.0/bastanagent.php
+++ plg_ajax_bastanagent v1.0.0/bastanagent.php
@@ -1,4 +1,9 @@
 <?php
+/**
+ * @package     BastanAgent
+ * @copyright   Copyright (C) 2026. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
 defined('_JEXEC') or die;
 use Joomla\CMS\Plugin\CMSPlugin;
 use Joomla\CMS\Factory;
